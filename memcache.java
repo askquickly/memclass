6e4f7db6e357985795173de706e80a516543a36c
J <askquickly@users.noreply.github.com>
2017-12-10 10:50:08 +0000

    Create memcache.java

---
+++ memcache.java
@@ -0,0 +1,25 @@
+package com.askquickly.memcache;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+mport anywheresoftware.b4a.BA;
+import anywheresoftware.b4a.IOnActivityResult;
+import anywheresoftware.b4a.BA.ActivityObject;
+import anywheresoftware.b4a.BA.DependsOn;
+import anywheresoftware.b4a.BA.Events;
+import anywheresoftware.b4a.BA.Permissions;
+import anywheresoftware.b4a.BA.ShortName;
+import anywheresoftware.b4a.BA.Version;
+import anywheresoftware.b4a.BA.Author;
+import anywheresoftware.b4a.keywords.Common;
+import anywheresoftware.b4a.objects.ViewWrapper;
+
+@Author("MemClass wrapped By J")
+@Version(0.01f)
+@ShortName("memcache")
+
+public class memcache implements StorageInterface
+
+}
